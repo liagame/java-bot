76a684b0cb0e139ee32ed9e6377e3179de6d5d09
svigelj <aljaz.svigelj@gmail.com>
2018-08-09 11:13:08 +0200

    Added comments in MyBot

--- src/MyBot.java
+++ src/MyBot.java
@@ -12,11 +12,14 @@ public class MyBot implements Callable {
     /** Called only once when the game is initialized. */
     @Override
     public synchronized void process(MapData mapData) {
+        // TODO write some code to handle mapData
     }
 
     /** Repeatedly called from game engine with game state updates.  */
     @Override
     public synchronized void process(StateUpdate stateUpdate, Api api) {
+        // TODO write some code to handle stateUpdate every frame.
+        // Use api to send responses back to the game engine.
     }
 
     public static void main(String[] args) throws Exception {
