5085ae86115034e6287592e064121fceeef8cb79
Svigelj <Svigelj@users.noreply.github.com>
2018-11-16 21:14:31 +0100

    Update GameState.java

--- src/lia/api/GameState.java
+++ src/lia/api/GameState.java
@@ -5,13 +5,16 @@ public class GameState {
     public MessageType type;
     public float time;
     public int numberOfRemainingOpponents;
+    public boolean canSaySomething;
     public UnitData[] units;
 
-    public GameState(long uid, MessageType type, float time, int numberOfRemainingOpponents, UnitData[] units) {
+    public GameState(long uid, MessageType type, float time, int numberOfRemainingOpponents, 
+                     boolean canSaySomething, UnitData[] units) {
         this.uid = uid;
         this.type = type;
         this.time = time;
         this.numberOfRemainingOpponents = numberOfRemainingOpponents;
+        this.canSaySomething = canSaySomething;
         this.units = units;
     }
 }
